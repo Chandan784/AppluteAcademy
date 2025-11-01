c2d4333a3e8894db55a5c2700ae1f445423853cc
chandan <chandansamantaray784@gmail.com>
2025-11-02 03:56:05 +0530

    quiz route added in navabr

--- src/components/Navabar.jsx
+++ src/components/Navabar.jsx
@@ -14,6 +14,7 @@ export default function Navbar() {
     { name: "About", path: "/about" },
     { name: "Feedback", path: "/feedback" },
     { name: "Contact", path: "/contact" },
+    { name: "Quiz", path: "/quiz" },
   ];
 
   return (
